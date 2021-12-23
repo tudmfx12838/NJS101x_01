cdb4b4051ea2f2a73d0435e512d9863e78d7ad6a
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2021-12-23 22:11:23 +0900

    Completed Lab1.4 chuyen huong request

--- NODEJS-COMPLETE-GUIDE/app.js
+++ NODEJS-COMPLETE-GUIDE/app.js
@@ -2,8 +2,17 @@ const http = require('http');
 
 const server = http.createServer((req,res) => {
     //console.log(req);//in toan bo thanh phan cua 1 request
-    console.log(req.url,req.method,req.headers);//in url, method, header
+    //console.log(req.url,req.method,req.headers);//in url, method, header
     //process.exit();
+    console.log(req.url);
+    const url = req.url;
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<head><title>My First Page</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name"=message"><button type="submit">Send</button></form></body>')
+        res.write('</html>');
+        return res.end();
+    }
     res.setHeader('Content-Type','text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
