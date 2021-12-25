061390b5d816dd4149eb1d5937291182291454b9
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2021-12-25 15:00:14 +0900

    Completed Lab1.7 Su dung he thong module Node

--- NODEJS-COMPLETE-GUIDE/app.js
+++ NODEJS-COMPLETE-GUIDE/app.js
@@ -1,42 +1,9 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req,res) => {
-    //console.log(req.url);
-    const url = req.url;
-    const method = req.method;
-    if(url === '/'){
-        res.write('<html>');
-        res.write('<head><title>My First Page</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message111"><button type="submit">Send</button></form></body>')
-        res.write('</html>');
-        return res.end();
-    }
-    if(url === '/message' && method === 'POST'){
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-        req.on('end', () => {
-            const parseBody = Buffer.concat(body).toString();
-            const message = parseBody.split('=')[1];
-            const readMessage = fs.readFileSync('message.txt') + '\n';
+const routes = require('./routes');
 
-            fs.writeFileSync('message.txt',readMessage + message, err => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
-            //console.log(parseBody);
-        });
-    }
-    res.setHeader('Content-Type','text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello from my Node.js Server!</h1></body>')
-    res.write('</html>');
-    res.end();
-});
+console.log(routes.someText);
+
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
