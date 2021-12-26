8b7e81a546abf09e8536a483bb8ed2dea4c195b1
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2021-12-27 00:13:44 +0900

    Completed Lab2.8 Them trang xu ly loi 404

--- NODEJS-COMPLETE-GUIDE/app.js
+++ NODEJS-COMPLETE-GUIDE/app.js
@@ -16,4 +16,8 @@ app.use(bodyPaser.urlencoded({extended: false}));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page Not Found</h1>');
+});
+
 app.listen(3000);
