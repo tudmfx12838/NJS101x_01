fc0fc35997e15d5e750d1598826d678190bbe5c8
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2021-12-26 23:32:08 +0900

    Completed Lab2.6 Gioi han viec thuc thi Middleware doi voi Post Requests

--- NODEJS-COMPLETE-GUIDE/app.js
+++ NODEJS-COMPLETE-GUIDE/app.js
@@ -14,13 +14,13 @@ app.use('/add-product',(req, res, next) => {
     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Prodcut</button></form>')
 });
 
-app.use('/product', (req, res, next) => {
+app.post('/product', (req, res, next) => {
     console.log(req.body)
     res.redirect('/');
 });
 
 app.use('/',(req, res, next) => {
-    console.log('In another midleware1!');
+    console.log('In another midleware!');
     res.send('<h1>Hello from Express!</h1>');
 });
 
