3bd60361bf1b707f2c16db6695d69257a8b990d9
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2021-12-27 00:07:55 +0900

    Completed Lab2.7 Su dung Express Router

--- NODEJS-COMPLETE-GUIDE/app.js
+++ NODEJS-COMPLETE-GUIDE/app.js
@@ -8,22 +8,12 @@ const bodyPaser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyPaser.urlencoded({extended: false}));
 
-app.use('/add-product',(req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Prodcut</button></form>')
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body)
-    res.redirect('/');
-});
-
-app.use('/',(req, res, next) => {
-    console.log('In another midleware!');
-    res.send('<h1>Hello from Express!</h1>');
-});
-
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
