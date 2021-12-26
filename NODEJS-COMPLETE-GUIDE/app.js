8d938fe8531475d8a5f48cfc8fd4855f052874f3
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2021-12-26 21:26:09 +0900

    Completed Lab2.2 Cai dat Midleware

--- NODEJS-COMPLETE-GUIDE/app.js
+++ NODEJS-COMPLETE-GUIDE/app.js
@@ -4,6 +4,15 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('In the midleware!');
+    next();//Allows the request to continue to next midleware in line
+});
+
+app.use((req, res, next) => {
+    console.log('In another midleware!');
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
