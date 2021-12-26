694d990f7ba3b2fa8f2ee79b3c00850fa6cac4f8
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2021-12-27 00:22:57 +0900

    Completed Lab2.9 Phan luong duong dan

--- NODEJS-COMPLETE-GUIDE/app.js
+++ NODEJS-COMPLETE-GUIDE/app.js
@@ -13,7 +13,7 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyPaser.urlencoded({extended: false}));
 
-app.use(adminRoutes);
+app.use('/admin/', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
