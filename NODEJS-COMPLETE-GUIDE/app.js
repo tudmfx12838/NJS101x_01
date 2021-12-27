d835d75bbe0fea395c0cc12816bafae314515122
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2021-12-28 01:01:54 +0900

    Completed Lab2.14 Cung cap tiep tinh

--- NODEJS-COMPLETE-GUIDE/app.js
+++ NODEJS-COMPLETE-GUIDE/app.js
@@ -1,24 +1,21 @@
-
 const path = require('path');
-//npm install --save express
-const express = require('express');
 
-//npm install --save body-parser
-const bodyPaser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyPaser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin/', adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    //res.status(404).send('<h1>Page Not Found</h1>');
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-
 });
+
 app.listen(3000);
