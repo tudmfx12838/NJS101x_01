0c9633cd840f9aaf0077452e861dfe3fab7e16f7
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2021-12-27 12:47:57 +0900

    Completed Lab2.11 Cung cap cac trang HTML

--- NODEJS-COMPLETE-GUIDE/routes/shop.js
+++ NODEJS-COMPLETE-GUIDE/routes/shop.js
@@ -1,10 +1,12 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/',(req, res, next) => {
     console.log('In another midleware!');
-    res.send('<h1>Hello from Express!</h1>');
+    res.sendFile(path.join(__dirname,'../', 'views', 'shop.html'));
 });
 
 
