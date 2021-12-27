5b0af2c5d54d762a9761fc104946f63fd9e02eb5
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2021-12-27 15:25:47 +0900

    Completed Lab2.12 Tra lai trang 404.html

--- NODEJS-COMPLETE-GUIDE/app.js
+++ NODEJS-COMPLETE-GUIDE/app.js
@@ -1,5 +1,5 @@
 
-
+const path = require('path');
 //npm install --save express
 const express = require('express');
 
@@ -17,7 +17,8 @@ app.use('/admin/', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page Not Found</h1>');
-});
+    //res.status(404).send('<h1>Page Not Found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 
+});
 app.listen(3000);
