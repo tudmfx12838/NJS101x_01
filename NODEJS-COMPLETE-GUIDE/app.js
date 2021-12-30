10401e1edca4340204d7c152d4652f17193a7a15
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2021-12-30 17:07:59 +0900

    Completed Lab3.3 Xuat noi dung su dung Pug

--- NODEJS-COMPLETE-GUIDE/app.js
+++ NODEJS-COMPLETE-GUIDE/app.js
@@ -7,6 +7,8 @@ const app = express();
 
 //Thiet lap templating engine by pug
 app.set('view engine', 'pug');
+//Thiet lap noi tim thay bo cuc thu muc views
+app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
--- NODEJS-COMPLETE-GUIDE/routes/shop.js
+++ NODEJS-COMPLETE-GUIDE/routes/shop.js
@@ -10,7 +10,8 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
   //console.log(adminData.products);
   //res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-  res.render('shop');
+  const products = adminData.products;
+  res.render('shop', {prods: products, docTitle: 'My Shop'});
 });
 
 module.exports = router;
