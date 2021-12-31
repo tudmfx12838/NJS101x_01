f9552c0eb2fd727fec22e38ebd2ddce334a07174
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2022-01-01 01:30:17 +0900

    Completed Lab4.5 Tim nap du lieu tu cac tiep thong qua model

--- NODEJS-COMPLETE-GUIDE/controllers/products.js
+++ NODEJS-COMPLETE-GUIDE/controllers/products.js
@@ -25,16 +25,18 @@ exports.getProducts = (req, res, next) => {
     //console.log(adminData.products);
     //res.sendFile(path.join(rootDir, 'views', 'shop.html'));
     //const products = adminData.products;
-    const products = Product.fetchAll();
-    res.render('shop', {
-      prods: products, 
-      docTitle: 'My Shop',
-      pageTitle: 'Shop', 
-      path: '/', hasProduct: products.length > 0,
-      activeShop: true,
-      activeAddProduct: false,
-      productCSS: true,
-      formCSS: false,
+    Product.fetchAll((products)=>{
+        res.render('shop', {
+        prods: products, 
+        docTitle: 'My Shop',
+        pageTitle: 'Shop', 
+        path: '/', hasProduct: products.length > 0,
+        activeShop: true,
+        activeAddProduct: false,
+        productCSS: true,
+        formCSS: false,
+      });
+
     });
 };
 
