63766bd15b267ce57e710806cbbb2cc571364ad0
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2022-01-03 23:00:08 +0900

    Completed Lab4.18 Xoa cac muc trong gio hang

--- NODEJS-COMPLETE-GUIDE/models/product.js
+++ NODEJS-COMPLETE-GUIDE/models/product.js
@@ -1,6 +1,8 @@
 //const products = [];
 const fs = require('fs');
 const path = require('path');
+const Cart = require('./cart');
+
 const p = path.join(
     path.dirname(process.mainModule.filename),
     'data',
@@ -50,10 +52,11 @@ module.exports = class Product {
     
     static deleteById(id){
       getProductsFromFile(products => {
+        const product = products.find(prod => prod.id === id);
         const updatedProduct = products.filter(prod => prod.id !== id);
         fs.writeFile(p, JSON.stringify(updatedProduct), err =>{
             if(!err){
-
+              Cart.deleteProduct(id, product.price);
             }
         });
       });
