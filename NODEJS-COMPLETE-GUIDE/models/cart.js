16d2a17ffd20c99243f106846a2a8298be8e68fd
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2022-01-04 01:04:22 +0900

    Completed Lab4.20 Xoa cac muc trong Gio Hang

--- NODEJS-COMPLETE-GUIDE/models/cart.js
+++ NODEJS-COMPLETE-GUIDE/models/cart.js
@@ -42,30 +42,33 @@ module.exports = class Cart {
 
     static deleteProduct(id, productPrice){
         fs.readFile(p, (err, fileContent) => {
-            if(!err){
-                return
-            }
-            
-            const updateCart = {...JSON.parse(fileContent)};
-            const product = updateCart.products.findIndex(prod => prod.id ===id);
-            const productQty = product.qty;
-            updateCart.products = updateCart.products.filter(prod => prod.id !== id);
-            updateCart.totalPrice = updateCart.totalPrice - productPrice * productQty;
-            
-            fs.writeFile(p, JSON.stringify(updateCart), err => {
-                console.log(err);
-            });
-        });  
-    }
-
-    static getCart(cb){
-        fs.readFile(p, (err, fileContent) => {
-            const cart = JSON.parse(fileContent);
             if(err){
-                cb([]);
-            }else{
-                cb(cart);
+                return;
             }
-        });
+            
+      const updatedCart = { ...JSON.parse(fileContent) };
+      const product = updatedCart.products.find(prod => prod.id === id);
+      if (!product) {
+          return;
+      }
+      const productQty = product.qty;
+      updatedCart.products = updatedCart.products.filter(prod => prod.id !== id);
+      updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty;
+
+      fs.writeFile(p, JSON.stringify(updatedCart), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static getCart(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      const cart = JSON.parse(fileContent);
+      if (err) {
+        cb(null);
+      } else {
+        cb(cart);
+      }
+    });
     }
 };
