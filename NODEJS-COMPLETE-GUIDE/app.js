9870bcbef3b991a27cbaab9d221ad213917c4c41
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2022-01-05 00:44:39 +0900

    Completed Lab5.1 Ket noi ung dung voi co so du lieu SQL

--- NODEJS-COMPLETE-GUIDE/app.js
+++ NODEJS-COMPLETE-GUIDE/app.js
@@ -13,9 +13,12 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
+const db = require('./util/database');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
+
+db.execute('SELECT * FROM products');
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
