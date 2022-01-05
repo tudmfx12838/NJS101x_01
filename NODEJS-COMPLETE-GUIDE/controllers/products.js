d5d9175173cac0cf2f49a52066048916ef3b8eac
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2022-01-05 22:58:33 +0900

    Completed Lab5.7 Tim nap mot san pham voi dieu kien WHERE

--- NODEJS-COMPLETE-GUIDE/controllers/products.js
+++ NODEJS-COMPLETE-GUIDE/controllers/products.js
@@ -1,42 +1,42 @@
-//const products = [];
-const Product = require("../models/product");
+// //const products = [];
+// const Product = require("../models/product");
 
-exports.getAddProduct = (req, res, next) => {
-    //res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-    res.render('add-product',{
-      pageTitle: 'Add Product',
-      path: '/admin/add-product',
-      activeShop: false,
-      activeAddProduct: true,
-      productCSS: true,
-      formCSS: true,
-    });
-};
+// exports.getAddProduct = (req, res, next) => {
+//     //res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+//     res.render('add-product',{
+//       pageTitle: 'Add Product',
+//       path: '/admin/add-product',
+//       activeShop: false,
+//       activeAddProduct: true,
+//       productCSS: true,
+//       formCSS: true,
+//     });
+// };
 
-exports.postAddProduct = (req, res, next) => {
-    //console.log(req.body);
-    //products.push({title: req.body.title});
-    const product = new Product(req.body.title);
-    product.save();
-    res.redirect('/');
-};
+// exports.postAddProduct = (req, res, next) => {
+//     //console.log(req.body);
+//     //products.push({title: req.body.title});
+//     const product = new Product(req.body.title);
+//     product.save();
+//     res.redirect('/');
+// };
 
-exports.getProducts = (req, res, next) => {
-    //console.log(adminData.products);
-    //res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-    //const products = adminData.products;
-    Product.fetchAll((products)=>{
-        res.render('shop', {
-        prods: products, 
-        docTitle: 'My Shop',
-        pageTitle: 'Shop', 
-        path: '/', hasProduct: products.length > 0,
-        activeShop: true,
-        activeAddProduct: false,
-        productCSS: true,
-        formCSS: false,
-      });
+// exports.getProducts = (req, res, next) => {
+//     //console.log(adminData.products);
+//     //res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+//     //const products = adminData.products;
+//     Product.fetchAll((products)=>{
+//         res.render('shop', {
+//         prods: products, 
+//         docTitle: 'My Shop',
+//         pageTitle: 'Shop', 
+//         path: '/', hasProduct: products.length > 0,
+//         activeShop: true,
+//         activeAddProduct: false,
+//         productCSS: true,
+//         formCSS: false,
+//       });
 
-    });
-};
+//     });
+// };
 
--- NODEJS-COMPLETE-GUIDE/models/product.js
+++ NODEJS-COMPLETE-GUIDE/models/product.js
@@ -31,7 +31,7 @@ module.exports = class Product {
     }
 
     static findById(id){
-
+      return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
     }
 
 };
