fc5e3e60f97bd7f47ce32d9af92e92316a3dd1f5
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2022-01-05 23:23:58 +0900

    Completed Lab5.8 Ket noi Sequelize voi Co So Du Lieu

--- NODEJS-COMPLETE-GUIDE/util/database.js
+++ NODEJS-COMPLETE-GUIDE/util/database.js
@@ -1,10 +1,19 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: '123456'
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'node-complete',
+//     password: '123456'
+// });
+
+// module.exports = pool.promise();
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete', 'root', '123456', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
