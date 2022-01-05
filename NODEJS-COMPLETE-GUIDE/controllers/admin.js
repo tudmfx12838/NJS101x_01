81eefbec55b62a757b2404136e42edf8a67a9582
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2022-01-05 22:49:05 +0900

    Completed Lab5.6 Chen du lieu vao co so du lieu

--- NODEJS-COMPLETE-GUIDE/controllers/admin.js
+++ NODEJS-COMPLETE-GUIDE/controllers/admin.js
@@ -14,8 +14,13 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(null, title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+  product
+    .save()
+    .then(()=>{
+      res.redirect('/');
+    })
+    .catch(err => console.log(err));
+  
 };
 
 exports.getProducts = (req, res, next) => {
@@ -64,10 +69,10 @@ exports.postEditProduct = (req, res, next) => {
                                     updatedPrice);
   updatedProduct.save();
   res.redirect('/admin/products');                   
-}
+};
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   Product.deleteById(prodId);
   res.redirect('/admin/products');
-}
+};
--- NODEJS-COMPLETE-GUIDE/models/product.js
+++ NODEJS-COMPLETE-GUIDE/models/product.js
@@ -15,7 +15,9 @@ module.exports = class Product {
   }
 
     save (){      
-
+      return db.execute('INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
+        [this.title, this.price, this.imageUrl, this.description]
+      );
     }
     
     static deleteById(id){
@@ -24,12 +26,12 @@ module.exports = class Product {
 
     static fetchAll(){
       return db.execute('SELECT * FROM products')
-        .then()
-        .catch();
+        //.then()
+        //.catch();
     }
 
     static findById(id){
-      
+
     }
 
 };
