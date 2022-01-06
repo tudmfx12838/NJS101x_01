67ee377000b94a61ae8b9a1a1a6bd9a891114a20
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2022-01-07 00:09:06 +0900

    Completed Lab5.19 Su dung phuong phap ket hop thong minh req.user.createProduct

--- NODEJS-COMPLETE-GUIDE/controllers/admin.js
+++ NODEJS-COMPLETE-GUIDE/controllers/admin.js
@@ -13,24 +13,24 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  // const product = new Product(null, title, imageUrl, description, price);
-  // product
-  //   .save()
-  //   .then(()=>{
-  //     res.redirect('/');
-  //   })
-  //   .catch(err => console.log(err));
-
-  Product.create({
-    title: title,
-    price: price,
-    imageUrl: imageUrl,
-    description: description
-  })
+  // Product.create({
+  //   title: title,
+  //   price: price,
+  //   imageUrl: imageUrl,
+  //   description: description,
+  //   userId: req.user.id
+  // })
+  req.user
+    .createProduct({
+      title: title,
+      price: price,
+      imageUrl: imageUrl,
+      description: description,
+    })
     .then(result => {
       //console.log(result);
       console.log('Created Product');
-      res.redirect('/');
+      res.redirect('/admin/products');
     })
     .catch(err =>{
        console.log(err);
