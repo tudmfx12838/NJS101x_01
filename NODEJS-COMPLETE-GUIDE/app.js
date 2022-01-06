de00f65a30089054fa4762e7046e47a5742b9273
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2022-01-06 23:18:04 +0900

    Completed Lab5.18 Tao va quan ly nguoi dung tam thoi

--- NODEJS-COMPLETE-GUIDE/app.js
+++ NODEJS-COMPLETE-GUIDE/app.js
@@ -14,19 +14,45 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    User
+        .findByPk(1)
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.getPageError);
 
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+
 sequelize
+    // .sync({force: true})//force: true . Se tu dong Drop table va tao lai
     .sync()
-    .then((result) => {
-        //console.log(result);
+    .then(result => {
+        return User.findByPk(1);
+    })
+    .then(user => {
+        if(!user){
+            return  User.create({name: 'Tu', email: 'tu@js.com'});
+        }
+
+        return user;
+    })
+    .then(user => {
+        //console.log(user);
         app.listen(3000);
     })
     .catch(err => {
