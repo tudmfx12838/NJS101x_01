a2de0155aaf20bba9bac2bad273489188560dd00
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2022-01-10 01:14:09 +0900

    Completed Lab6.1 Cai dat trinh dieu khien MongoDB

--- NODEJS-COMPLETE-GUIDE/util/database.js
+++ NODEJS-COMPLETE-GUIDE/util/database.js
@@ -9,11 +9,27 @@
 
 // module.exports = pool.promise();
 
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', '123456', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
+// const sequelize = new Sequelize('node-complete', 'root', '123456', {
+//     dialect: 'mysql',
+//     host: 'localhost'
+// });
 
-module.exports = sequelize;
+// module.exports = sequelize;
+
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const mongoConnect = (callback) => {
+    MongoClient.connect('mongodb+srv://admin:yYoPp04jYzN8lHf7@cluster0.gqluc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+    .then(client => {
+        console.log('Connected!!!');
+        callback(client);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+module.exports = mongoConnect;
