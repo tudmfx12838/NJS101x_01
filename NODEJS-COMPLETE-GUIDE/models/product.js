8f9f28894b4d16cc4035d613b59d60856e958f30
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2022-01-10 13:54:21 +0900

    Completed Lab6.11 Mot luu y ve viec update San Pham( ObjectId )

--- NODEJS-COMPLETE-GUIDE/models/product.js
+++ NODEJS-COMPLETE-GUIDE/models/product.js
@@ -7,7 +7,7 @@ class Product {
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
-    this._id = id;
+    this._id = new mongodb.ObjectId(id);
   }
   save() {
     const db = getDb();
@@ -16,7 +16,7 @@ class Product {
       //existing _id => Update product
       dbOp = db
         .collection("products")
-        .updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: this });
+        .updateOne({ _id: this._id }, { $set: this });
     } else {
       dbOp = db.collection("products").insertOne(this);
     }
