2af296267d34c35aea63b14f49303e18fa5a477c
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2022-01-10 17:38:56 +0900

    Completed Lab6.16 Lam viec tren cac don hang va don dat hang trong gio hang

--- NODEJS-COMPLETE-GUIDE/models/user.js
+++ NODEJS-COMPLETE-GUIDE/models/user.js
@@ -2,9 +2,11 @@ const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
+    this.cart = cart; // {item: []}
+    this._id = id;
   }
 
   save() {
@@ -12,16 +14,29 @@ class User {
     return db.collection("users").insertOne(this);
   }
 
+  addToCart(product) {
+    // const cartProduct = this.cart.item.findIndex(cp => {
+    //     return cp._id === product._id;
+    // });
+
+    const updateCart = { item: [{ ...product, quantity: 1 }] };
+    const db = getDb();
+    db.collection("users").updateOne(
+      { _id: new mongodb.ObjectId(this._id) }, 
+      {$set: {cart: updateCart}
+    });
+  }
+
   static findById(userId) {
     const db = getDb();
     return db
       .collection("users")
       .findOne({ _id: new mongodb.ObjectId(userId) })
-      .then(user => {
+      .then((user) => {
         console.log(user);
         return user;
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
   }
