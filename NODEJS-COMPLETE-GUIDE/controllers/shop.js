abfbcb1be75016fa0cce89199abcdfbc490eec5c
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2022-01-11 21:59:07 +0900

    Completed Lab6.24 Nhan don dat hang

--- NODEJS-COMPLETE-GUIDE/controllers/shop.js
+++ NODEJS-COMPLETE-GUIDE/controllers/shop.js
@@ -120,7 +120,7 @@ exports.postOrder = (req, res, next) => {
 
 exports.getOrders = (req, res, next) => {
   req.user 
-    .getOrders({ include: ['products']})
+    .getOrders()
     .then(orders => {
       console.log(orders);
       res.render('shop/orders', {
