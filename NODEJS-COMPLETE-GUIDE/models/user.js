fbf90d60f8ee7a3155531f652a410f5ce4ebc7f6
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2022-01-12 22:22:49 +0900

    Completed Lab7.13 Xoa cac muc trong gio hang Cart

--- NODEJS-COMPLETE-GUIDE/models/user.js
+++ NODEJS-COMPLETE-GUIDE/models/user.js
@@ -54,6 +54,22 @@ userSchema.methods.addToCart = function(product) { //tao function bang ownerShem
     //   );
 };
 
+userSchema.methods.removeFromCart = function(productId) {
+  //  const updatedCartItems = [...this.cart.items];
+    const updatedCartItems = this.cart.items.filter(item => {
+      return item.productId.toString() !== productId.toString();
+    });
+    this.cart.items = updatedCartItems;
+    return this.save();
+    // const db = getDb();
+    // return db
+    //   .collection("users")
+    //   .updateOne(
+    //     { _id: new ObjectId(this._id) },
+    //     { $set: { cart: { items: updatedCartItems } } }
+    //   );
+}
+
 module.exports = mongoose.model('User', userSchema);
 
 // const mongodb = require("mongodb");
