c98c48acc0742caf7ca1d1b4e6e9a02f46e6663d
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2022-01-12 10:36:02 +0900

    Completed Lab7.8 Them va su dung model User

--- NODEJS-COMPLETE-GUIDE/app.js
+++ NODEJS-COMPLETE-GUIDE/app.js
@@ -1,49 +1,63 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const errorController = require('./controllers/error');
-// const mongoConnect = require('./util/database').mongoConnect;
+const errorController = require("./controllers/error");
+const User = require("./models/user");
 
-// const User = require('./models/user');
 const app = express();
 
 //Thiet lap templating engine by EJS
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 //Thiet lap noi tim thay bo cuc thu muc views
-app.set('views', 'views');
+app.set("views", "views");
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-    // User
-    //     .findById('61dc48a8ee456c981655d79a')
-    //     .then(user => {
-    //         // req.user = user; //user lay tu database
-    //         req.user = new User(user.name, user.email, user.cart, user._id); //user lay tu database va khoi tao 1 doi tuong User de xu ly
-    //         next();
-    //     })
-    //     .catch(err => console.log(err));
-   next();
+  User.findById("61de3047734cb18074b8da08")
+    .then((user) => {
+      // req.user = user; //user lay tu database
+      req.user = user; //user nay tra ve tu findById (ham cua mongoose) lay tu database
+      next();
+    })
+    .catch((err) => console.log(err));
+  //    next();
 });
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.getPageError);
 
-mongoose.connect('mongodb+srv://admin:yYoPp04jYzN8lHf7@cluster0.gqluc.mongodb.net/myShop?retryWrites=true&w=majority')
-.then(result => {
+mongoose
+  .connect(
+    "mongodb+srv://admin:yYoPp04jYzN8lHf7@cluster0.gqluc.mongodb.net/myShop?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    User
+        .findOne() //findOne cua mongoose luon tra ve 1 user dau tien trong collection users
+        .then((user) => {
+            if(!user){ //if user not exist, add new user.
+                const user = new User({
+                    name: "Tu",
+                    email: "test@node",
+                    cart: {
+                      items: [],
+                    },
+                  });
+                user.save();
+            }
+        });
+
     app.listen(3000);
-})
-.catch(err => {
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
