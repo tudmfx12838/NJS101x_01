04bbc56e0ddffdabf4ad25771016b8ebcc7e0dfd
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2022-01-25 23:06:34 +0900

    Completed Lab8.2 Thiet lap Cookie

--- NODEJS-COMPLETE-GUIDE/controllers/auth.js
+++ NODEJS-COMPLETE-GUIDE/controllers/auth.js
@@ -1,14 +1,19 @@
-
 exports.getLogin = (req, res, next) => {
-    res.render("auth/login", {
-        path: "/login",
-        pageTitle: "Login",
-        isAuthenticated: req.isAuthenticated
-    });
+  const isLoggedIn = req
+    .get("Cookie")
+    .split(";")[0]
+    .trim()
+    .split("=")[1];
+
+  console.log(isLoggedIn);
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    isAuthenticated: isLoggedIn,
+  });
 };
 
 exports.postLogin = (req, res, next) => {
-    req.isLoggedIn
-    res.redirect('/');
+  res.setHeader("Set-Cookie", "loggedIn=true");
+  res.redirect("/");
 };
-
