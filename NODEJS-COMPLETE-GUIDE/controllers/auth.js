9bf173a49b87c0bc87325793a9ebb7adeea43cc7
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2022-01-30 18:58:50 +0900

    Lab9.8 Them css cho input invalid

--- NODEJS-COMPLETE-GUIDE/controllers/auth.js
+++ NODEJS-COMPLETE-GUIDE/controllers/auth.js
@@ -42,7 +42,8 @@ exports.getSignup = (req, res, next) => {
     path: "/signup",
     pageTitle: "Signup",
     errorMessage: message,
-    oldInput: {email: "", password: ""}
+    oldInput: {email: "", password: ""},
+    validationErrors:  []
   });
 };
 exports.postLogin = (req, res, next) => {
@@ -102,7 +103,8 @@ exports.postSignup = (req, res, next) => {
       path: "/signup",
       pageTitle: "Signup",
       errorMessage: errors.array()[0].msg,
-      oldInput: {email: email, password: password}
+      oldInput: {email: email, password: password},
+      validationErrors:  errors.array()
     });
   }
 
