840f800dd83079bdcb6acb83eeaef4795b642837
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2022-01-30 18:40:33 +0900

    Lab9.7 Giu thong tin Input cua nguoi dung

--- NODEJS-COMPLETE-GUIDE/controllers/auth.js
+++ NODEJS-COMPLETE-GUIDE/controllers/auth.js
@@ -42,11 +42,23 @@ exports.getSignup = (req, res, next) => {
     path: "/signup",
     pageTitle: "Signup",
     errorMessage: message,
+    oldInput: {email: "", password: ""}
   });
 };
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render("auth/login", {
+      path: "/login",
+      pageTitle: "Login",
+      errorMessage: errors.array()[0].msg,
+      })
+  }
 
   User.findOne({ email: email })
     .then((user) => {
@@ -90,6 +102,7 @@ exports.postSignup = (req, res, next) => {
       path: "/signup",
       pageTitle: "Signup",
       errorMessage: errors.array()[0].msg,
+      oldInput: {email: email, password: password}
     });
   }
 
--- NODEJS-COMPLETE-GUIDE/routes/auth.js
+++ NODEJS-COMPLETE-GUIDE/routes/auth.js
@@ -2,7 +2,9 @@ const express = require("express");
 const { check, body } = require("express-validator/check");
 
 const authController = require("../controllers/auth");
-const User = require('../models/user');
+const User = require("../models/user");
+
+const bcrypt = require("bcryptjs");
 
 const router = express.Router();
 
@@ -10,7 +12,17 @@ router.get("/login", authController.getLogin);
 
 router.get("/signup", authController.getSignup);
 
-router.post("/login", authController.postLogin);
+router.post(
+  "/login",
+  [
+    body("email")
+      .isEmail()
+      .withMessage("Please enter a valid email address"),
+    body('password', 'Password has to be valid')
+    .isLength({min: 5}).isAlphanumeric()
+  ],
+  authController.postLogin
+);
 
 router.post(
   "/signup",
@@ -25,7 +37,8 @@ router.post(
         // return true;
         return User.findOne({ email: value }).then((userDoc) => {
           if (userDoc) {
-            return Promise.reject( //them loi xac thuc khong dong bo
+            return Promise.reject(
+              //them loi xac thuc khong dong bo
               "Email exist already, please pick  a different one!"
             );
           }
