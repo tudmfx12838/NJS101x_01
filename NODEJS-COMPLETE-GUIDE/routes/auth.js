b3837a35e7f893b99ed0242cc7ae347377a2b364
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2022-01-30 15:56:10 +0900

    Lab9.4 Them trinh kiem tra du lieu password

--- NODEJS-COMPLETE-GUIDE/routes/auth.js
+++ NODEJS-COMPLETE-GUIDE/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { check } = require("express-validator/check");
+const { check, body } = require("express-validator/check");
 
 const authController = require("../controllers/auth");
 
@@ -13,16 +13,26 @@ router.post("/login", authController.postLogin);
 
 router.post(
   "/signup",
-  check("email")
-    .isEmail()
-    .withMessage("Please enter a valid email")
-    .custom((value, { req }) => {
-        if (value === 'test@test.com') {
-            throw new Error ('This email address if forbidden.');
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter a valid email")
+      .custom((value, { req }) => {
+        if (value === "test@test.com") {
+          throw new Error("This email address if forbidden.");
         }
-        
+
         return true;
-    }),
+      }),
+    body(
+      "password",
+      "Please enter a password with only numbers and text and at least 5 characters."
+    ).isLength({ min: 5 })
+    .isAlphanumeric(),
+    //   .withMessage(
+    //     "Please enter a password with only numbers and text and at least 5 characters."
+    //   ),
+  ],
   authController.postSignup
 );
 
